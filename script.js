437e056b662fa6708275bd4237801624992a6511
Ahmed Talaat <ahmed.talaat@creiden.com>
2023-10-22 23:49:29 +0300

    add finalized js

--- script.js
+++ script.js
@@ -1,28 +1,115 @@
 // When option checked, show input area
-function showHiddenFields(id, actionId, textCheck = null, select = true) {
+const showHiddenFields = (id, actionId, textCheck = null, select = false) => {
   const element = document.getElementById(id);
   const action = document.getElementById(actionId);
-  switch (select) {
-    case true:
-      if (element.value == textCheck) {
+  if (select)
+    if (element.value == textCheck) {
+      action.style.display = "block";
+    } else {
+      action.style.display = "none";
+    }
+  else {
+    switch (actionId) {
+      case "otherBoxTwo":
         action.style.display = "block";
-      } else {
-        action.style.display = "none";
-      }
-    case false:
-      toggleAttr(element);
-      if (element.hasAttribute("sinkOff")) {
-        action.style.display = "block";
-      } else {
-        action.style.display = "none";
-      }
+        break;
+      case "otherBoxThree":
+        if (element.value == "To be installed") {
+          action.style.display = "block";
+        } else if (element.value == "Already Installed") {
+          action.style.display = "none";
+        }
+        break;
+      case "tearText":
+        if (element.value == "yes") {
+          toggleDisabled(action, "tearNo", "tearNo");
+        } else {
+          toggleDisabled(action, "tearYes", "tearYes");
+        }
+        break;
+      case "checkText":
+        if (element.value == "no") {
+          toggleDisabled(action, "checkText", "svgOn");
+        } else {
+          toggleDisabled(action, "", "svgOff");
+        }
+        break;
+      case "checkFaucet":
+        if (element.value == "yes") {
+          toggleDisabled(action, "faucetNo", "faucetNo");
+        } else {
+          toggleDisabled(action, "faucetYes", "faucetYes");
+        }
+        break;
+    }
   }
-}
+};
 
-function toggleAttr(element) {
-  if (element.hasAttribute("sinkOff")) {
-    element.removeAttribute("sinkOff");
-  } else {
-    element.setAttribute("sinkOff", true);
+const toggleDisabled = (action, id, disableId) => {
+  const disableElement = document.getElementById(disableId);
+  if (action) {
+    if (
+      action.style.display === "none" &&
+      (id == "tearNo" || id == "checkText")
+    ) {
+      action.style.display = "block";
+    } else if (action.style.display === "block") action.style.display = "none";
   }
-}
+  disableElement.disabled = !disableElement.disabled;
+};
+
+const checkForSelected = () => {
+  const cabinetOptions = document.querySelectorAll("option.cabinets");
+  document.getElementById("install").addEventListener("change", function () {
+    const selectedOption = cabinetOptions[this.selectedIndex - 1];
+    if (selectedOption) {
+      const selectedValue = selectedOption.value;
+      if (selectedValue == "To be installed") {
+        console.log("test");
+      }
+    }
+  });
+};
+
+// ----------------------------------- Add Email Js Details ------------------------------------------- //
+
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("_mtDuxd9bNq3NiHj2");
+})();
+document
+  .getElementById("customer-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // if (
+    //   nameInput.value === "" ||
+    //   emailInput.value === "" ||
+    //   messageInput.value === ""
+    // ) {
+    //   toastr.error("Please fill in all required fields.", "Error", {
+    //     positionClass: "toast-top-right",
+    //     backgroundColor: "#ff0000",
+    //   });
+    //   return;
+    // }
+
+    // these IDs from the previous steps
+    emailjs.sendForm("service_9y1ve0g", "template_h7enjtb", this).then(
+      function () {
+        toastr.success(
+          "Thank you for filling out the form, your Data has been saved!",
+          "Success",
+          {
+            positionClass: "toast-top-right",
+            // backgroundColor: "#ff0000",
+          }
+        );
+      },
+      function (error) {
+        toastr.error("Something went wrong!", "Error", {
+          positionClass: "toast-top-right",
+        });
+      }
+    );
+  });
